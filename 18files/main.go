4b3882800c588ff553c94cebbd466fc24dcd67c6
Prajapati omkar <prajapatiomkar9833@gmail.com>
2023-01-18 18:07:24 +0530

    Day 18 for GoLang with LOC

---
+++ 18files/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	fmt.Println("Welcome to files in golang")
+	content := "This needs to go in a file - Sample.in"
+	file, err := os.Create("./mylcogofile.txt")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+	checkNilErr(err)
+	length, err := io.WriteString(file, content)
+
+	checkNilErr(err)
+	
+	fmt.Println("length is: ", length)
+
+	defer file.Close()
+
+	readFile("./mylcogofile.txt")
+}
+
+func readFile(filename string) {
+	databyte, err := ioutil.ReadFile(filename)
+
+	checkNilErr(err)
+
+	fmt.Println("Text data inside the file is \n", string(databyte))
+}
+// To avoid repetition
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
