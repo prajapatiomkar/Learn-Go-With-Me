c8aceaf28b028f702bd8885dd74b75ef7e6d375c
Prajapati omkar <prajapatiomkar9833@gmail.com>
2023-01-19 13:55:37 +0530

    Day 20 for GoLang with LOC

--- 20urls/main.go
+++ 20urls/main.go
@@ -20,6 +20,7 @@ func main() {
 	fmt.Println(result.Host)
 	fmt.Println(result.Path)
 	fmt.Println(result.RawQuery)
+	
 	// fmt.Println(result.Port())
 	qparams := result.Query()
 	fmt.Printf("The type of query params are: %T\n",qparams)
